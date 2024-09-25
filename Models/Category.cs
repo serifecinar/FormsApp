4e21150fb00588a6082d9057d8ee2105fad9c26b
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-25 15:14:15 +0300

    first commit

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FormsApp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
