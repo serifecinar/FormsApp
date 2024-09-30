bdfd588afa7cb803c2aab791cdcc6790a6a7390e
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-30 16:00:42 +0300

    Forms-Categories select

--- obj/Debug/net8.0/FormsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f197ff1531a0d9319c0eef5f59e44e7c6ce5bc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f9f8e824772f07d869e687ceae3e36a7c501f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
